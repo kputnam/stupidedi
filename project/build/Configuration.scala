2da2873068154c395e7f8bc4ba2fe2bfcc792d35
kputnam <putnam.kyle@gmail.com>
2010-11-15 18:11:55 -0600

    Upgrade to Scala 2.8.1

--- project/build/Configuration.scala
+++ project/build/Configuration.scala
@@ -5,8 +5,8 @@ class Configuration(info: ProjectInfo) extends DefaultProject(info) {// with de.
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at
     "http://scala-tools.org/repo-snapshots"
 
-  val specs      = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"
-  val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.0" % "1.7" % "test"
+  val specs      = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test"
+  val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.1" % "1.8" % "test"
   val postgres   = "postgresql" % "postgresql" % "8.4-701.jdbc4"
 
   override def compileOptions = Optimise :: ExplainTypes :: Unchecked :: super.compileOptions.toList
