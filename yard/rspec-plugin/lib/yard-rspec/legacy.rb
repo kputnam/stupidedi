0caba4647cb7066b26c034ef2d8ef2f3f2d73373
kputnam <putnam.kyle@gmail.com>
2011-01-11 16:23:39 +0000

    Updated documentation (@group is a nice feature)
    Finally fixed DelegatedInput#drop and FileInput#drop to update the
    column, and defaulted column and line in the constructors to 1
    
    Fixed minor yard-rspec handler bug for Ruby 1.8

--- yard/rspec-plugin/lib/yard-rspec/legacy.rb
+++ yard/rspec-plugin/lib/yard-rspec/legacy.rb
@@ -39,7 +39,6 @@ end
 
 class LegacyRSpecItHandler < YARD::Handlers::Ruby::Legacy::Base
   MATCH = /\A(?:its?|specify)\s+(['"])(.+?)\1(?:.*?)\s+(?:do|\{)/
-
   handles MATCH
   handles /\A(?:its?|specify)\s+(?:do|\{)/
   
