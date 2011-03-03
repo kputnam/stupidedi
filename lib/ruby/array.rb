14f29f05d13ae935af88fd1e4a5d2d8a611ef93d
kputnam <putnam.kyle@gmail.com>
2011-03-03 16:02:55 +0000

    Added return type declarations to Array methods

--- lib/ruby/array.rb
+++ lib/ruby/array.rb
@@ -8,23 +8,27 @@ class Array
   end
 
   # Selects all elements except the first
+  # @return [Array]
   def tail
     _, *xs = self
     xs
   end
 
   # Selects all elements except the last
+  # @return [Array]
   def init
     slice(0..-2) or []
   end
 
   # Select all elements except the first +n+ ones
+  # @return [Array]
   def drop(n)
     raise ArgumentError, "n (#{n}) must be positive" if n < 0
     slice(n..-1) or []
   end
 
   # Drops the longest prefix of elements that satisfy the predicate
+  # @return [Array]
   def drop_while(&block)
     # This is in tail call form
     if not empty? and yield(head)
@@ -35,6 +39,7 @@ class Array
   end
 
   # Drops the longest prefix of elements that do not satisfy the predicate
+  # @return [Array]
   def drop_until(&block)
     # This is in tail call form
     unless empty? or yield(head)
@@ -45,12 +50,14 @@ class Array
   end
 
   # Select all elements except the last +n+ ones
+  # @return [Array]
   def take(n)
     raise ArgumentError, "n (#{n}) must be positive" if n < 0
     slice(0, n) or []
   end
 
   # Takes the longest prefix of elements that satisfy the predicate
+  # @return [Array]
   def take_while(accumulator = [], &block)
     # This is in tail call form
     if not empty? and yield(head)
@@ -61,6 +68,7 @@ class Array
   end
 
   # Takes the longest prefix of elements that do not satisfy the predicate
+  # @return [Array]
   def take_until(accumulator = [], &block)
     # This is in tail call form
     unless empty? or yield(head)
@@ -71,6 +79,7 @@ class Array
   end
 
   # Splits the array into prefix/suffix pair according to the predicate
+  # @return [(Array, Array)]
   def span(&block)
     prefix = take_while(&block)
     suffix = drop(prefix.length)
@@ -78,6 +87,7 @@ class Array
   end
 
   # Splits the array into prefix/suffix pair according to the predicate
+  # @return [(Array, Array)]
   def split_when(&block)
     prefix = take_until(&block)
     suffix = drop(prefix.length)
@@ -85,6 +95,7 @@ class Array
   end
 
   # Split the array in two at the given position
+  # @return [(Array, Array)]
   def split_at(n)
     return take(n), drop(n)
   end
