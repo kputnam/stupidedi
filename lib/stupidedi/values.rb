d505638e31f14e1b397dab79fba9b4a70a8d85ef
kputnam <putnam.kyle@gmail.com>
2011-03-07 23:50:57 +0000

    Removed autoload statements for classes that since have moved elsewhere

--- lib/stupidedi/values.rb
+++ lib/stupidedi/values.rb
@@ -6,14 +6,7 @@ module Stupidedi
     autoload :RepeatedElementVal,   "stupidedi/values/repeated_element_val"
     autoload :SegmentVal,           "stupidedi/values/segment_val"
     autoload :SegmentValGroup,      "stupidedi/values/segment_val_group"
+    autoload :SimpleElementVal,     "stupidedi/values/simple_element_val"
     autoload :TableVal,             "stupidedi/values/table_val"
-
-    autoload :SimpleElementVal, "stupidedi/values/simple_element_val"
-    autoload :DateVal,          "stupidedi/values/date_val"
-    autoload :DecimalVal,       "stupidedi/values/decimal_val"
-    autoload :IdentifierVal,    "stupidedi/values/identifier_val"
-    autoload :NumericVal,       "stupidedi/values/numeric_val"
-    autoload :StringVal,        "stupidedi/values/string_val"
-    autoload :TimeVal,          "stupidedi/values/time_val"
   end
 end
