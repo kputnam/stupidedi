2c61397172a42b19782adac51aa4c0386ec1e2af
kputnam <putnam.kyle@gmail.com>
2011-03-24 20:02:07 +0000

    Fixed incorrect repeat count for elements in AK9, IK4, and IK5 segments. The X231 FA999 implementation guide indicates these are non-repeating elements, but I'm not certain if that's an implementation detail or if the X12 segment is defined that way also. I don't recall any other references to these segments from other implementation guides, so I suspect it was a typo from the beginning.

--- lib/stupidedi/dictionaries/functional_groups/005010/segment_defs/AK9.rb
+++ lib/stupidedi/dictionaries/functional_groups/005010/segment_defs/AK9.rb
@@ -10,7 +10,7 @@ module Stupidedi
 
           AK9 = s::SegmentDef.build(:AK9, "Functional Group Response Trailer",
             "To acknowledge acceptance or rejection of a functional group and report the number of included transaction sets from the original trailer, the accepted sets, and the received sets in this functional group",
-            e::E715 .simple_use(r::Mandatory,  s::RepeatCount.bounded(10)),
+            e::E715 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
             e::E97  .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
             e::E123 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
             e::E2   .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
--- lib/stupidedi/dictionaries/functional_groups/005010/segment_defs/IK4.rb
+++ lib/stupidedi/dictionaries/functional_groups/005010/segment_defs/IK4.rb
@@ -10,7 +10,7 @@ module Stupidedi
 
           IK4 = s::SegmentDef.build(:IK4, "Implementation Data Element Note",
             "To report implementation errors in a data element or composite data structure and identify the location of the data element",
-            e::C030 .simple_use(r::Mandatory,  s::RepeatCount.bounded(10)),
+            e::C030 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
             e::E725 .simple_use(r::Optional,   s::RepeatCount.bounded(1)),
             e::E621 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
             e::E724 .simple_use(r::Optional,   s::RepeatCount.bounded(1)))
