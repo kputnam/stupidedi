b3b918f9df98bffddcaa917958973d3981ccccaa
kputnam <putnam.kyle@gmail.com>
2011-06-20 16:23:38 +0000

    Cleaned up some edits... still not sure how to best organize these.

--- lib/stupidedi/editor/X222-HC837.rb
+++ lib/stupidedi/editor/X222-HC837.rb
@@ -15,7 +15,16 @@ module Stupidedi
       end
 
       def critique(st, acc)
-        acc.tap { critique_st(st, acc) }
+        acc.tap do
+          critique_st(st, acc)
+
+        # st.find(:SE).tap do |se|
+        #   st.distance(se).tap do |n|
+        #     n.times { st.next.tap{|z| z }}
+        #   end
+        # end
+
+        end
       end
 
     private
@@ -68,6 +77,7 @@ module Stupidedi
           gs.parent.tap do |isa|
             critique_isa(isa, acc)
           end
+
           critique_gs(gs, acc)
         end
       end
@@ -91,6 +101,93 @@ module Stupidedi
       # X222.087.2010AA.NM109.030
       # X222.087.2010AA.NM109.040
       # X222.087.2010AA.N302.060  * "Post Office Box", "P.O. Box", "PO Box", "Lock Box", "Lock Bin"
+
+      def critique_nm1(nm1, acc)
+        edit(:NM1) do
+          # Organization/last name
+          nm1.element(3).tap do |e|
+            if e.node.blank? and e.node.usage.optional?
+              acc.warn(e, "optional element is not present")
+            end
+          end
+
+          # Non-person entity
+          if nm1.element(2).select{|e| e.node == "2" }.defined?
+            # First name
+            nm1.element(4).tap do |e|
+              if e.node.present? and e.node.usage.optional?
+                acc.stc01(e, "T", "A8", "505", "must not be present when NM102 is 2")
+              end
+            end
+
+            # Middle name
+            nm1.element(5).tap do |e|
+              if e.node.present? and e.node.usage.optional?
+                acc.stc01(e, "T", "A8", "514", "must not be present when NM102 is 2")
+              end
+            end
+
+            # Prefix name
+            nm1.element(6).tap do |e|
+              if e.node.present? and e.node.usage.optional?
+                acc.stc01(e, "T", "A8", "125", "must not be present when NM102 is 2")
+              end
+            end
+
+            # Suffix name
+            nm1.element(7).tap do |e|
+              if e.node.present? and e.node.usage.optional?
+                acc.stc01(e, "T", "A8", "125", "must not be present when NM102 is 2")
+              end
+            end
+          end
+
+          # Person
+          if nm1.element(2).select{|e| e.node == "1" }.defined?
+            # First name
+            nm1.element(4).tap do |e|
+              if e.node.blank? and e.node.usage.optional?
+                acc.warn(e, "optional element is not present")
+              end
+            end
+
+            # Middle name
+            nm1.element(5).tap do |e|
+              if e.node.blank? and e.node.usage.optional?
+                acc.warn(e, "optional element is not present")
+              end
+            end
+
+            # Prefix name
+            nm1.element(6).tap do |e|
+              if e.node.blank? and e.node.usage.optional?
+                acc.warn(e, "optional element is not present")
+              end
+            end
+
+            # Suffix name
+            nm1.element(7).tap do |e|
+              if e.node.blank? and e.node.usage.optional?
+                acc.warn(e, "optional element is not present")
+              end
+            end
+          end
+        end
+      end
+
+      def critique_n3(n3, acc)
+        edit(:N3) do
+          n3.element(2).tap do |e|
+            if e.node.usage.optional?
+              unless n3.element(1).reject(&:blank?).defined?
+                # Second address line (N302) shouldn't be present if the
+                # first (N301) isn't also present
+                acc.warn(e, "second address line present without first line")
+              end
+            end
+          end
+        end
+      end
 
     end
 
