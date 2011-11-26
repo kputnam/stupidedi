e5048e25814a7a6a22301464cab023939afa3aa3
kputnam <putnam.kyle@gmail.com>
2011-11-26 00:34:18 -0600

    Really don't pollute global namespace

--- spec/support/rcov.rb
+++ spec/support/rcov.rb
@@ -7,30 +7,28 @@
 # a new namespace in which to define the method, this is a defined as a lambda
 # value instead of a method.
 #
-loader = lambda do |root|
-  # Prevent walking in circles due to cycles in the graph
-  # like Object.constants.include?("Object") #=> true
-  history = Hash.new
-
-  lambda do |namespace, recurse|
-    history[namespace] = true
-
-    for child in namespace.constants
-      begin
-        value = namespace.const_get(child)
-
-        if value.is_a?(Module) and not history[value]
-          recurse.call(value, recurse)
-        end
-      rescue LoadError, NameError
-        $stderr.puts "warning: #{$!.class} #{$!.message}"
-      end
-    end
-  end.bind do |recurse|
-    recurse.call(root, recurse)
-  end
-end
-
 if defined? Rcov
-  loader.call(Stupidedi)
+  lambda do |root|
+    # Prevent walking in circles due to cycles in the graph
+    # like Object.constants.include?("Object") #=> true
+    history = Hash.new
+
+    lambda do |namespace, recurse|
+      history[namespace] = true
+
+      for child in namespace.constants
+        begin
+          value = namespace.const_get(child)
+
+          if value.is_a?(Module) and not history[value]
+            recurse.call(value, recurse)
+          end
+        rescue LoadError, NameError
+          $stderr.puts "warning: #{$!.class} #{$!.message}"
+        end
+      end
+    end.bind do |recurse|
+      recurse.call(root, recurse)
+    end
+  end.call(Stupidedi)
 end
