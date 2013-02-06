5b23e52365234755c46de51ca458835763083693
Nathan Smith <nathansmith22@gmail.com>
2013-02-05 19:11:43 -0500

    RE944 -- initial commit.

---
+++ lib/stupidedi/versions/functional_groups/004010/transaction_set_defs/RE944.rb
@@ -0,0 +1,42 @@
+module Stupidedi
+  module Versions
+    module FunctionalGroups
+      module FortyTen
+        module TransactionSetDefs
+          d = Schema
+          r = SegmentReqs
+          s = SegmentDefs
+
+          RE944 =  d::TransactionSetDef.build("RE", "944", "Warehouse Stock Transfer Receipt Advice",
+            
+            d::TableDef.header("Table 1 - Header",
+              s::ST.use( 10, r::Mandatory, d::RepeatCount.bounded(1)),
+              s::ST.use(20, r::Mandatory, d::RepeatCount.bounded(1)),
+              d::LoopDef.build("N1", d::RepeadCount.bounded(10),
+                s::N1.use( 140, r::Mandatory,  d::RepeatCount.bounded(1)),
+                s::N1.use( 140, r::Mandatory,  d::RepeatCount.bounded(1))
+              ),
+              s::W08.use(130, r::Mandatory ,  d::RepeatCount.bounded(1))
+            ),
+
+            d::TableDef.header("Table 2 - Detail",
+              d::LoopDef.build("W07", d::RepeatCount.bounded(9999),
+                s::W07.use(220, r::Mandatory, d::RepeatCount.bounded(1)),
+                s::G69.use(230, r::Mandatory, d::RepeatCount.bounded(5)),
+                s::N9.use(240, r::Mandatory, d::RepeatCount.bounded(200)),
+                s::N9.use(240, r::Mandatory, d::RepeatCount.bounded(200))
+              )
+            ),
+
+            d::TableDef.header("Table 3 - Summary",
+              s::W14.use( 10, r::Mandatory,  d::RepeatCount.bounded(1)),
+              s::SE.use(30, r::Mandatory, d::RepeatCount.bounded(1))
+            )
+          )
+        end
+      end
+    end
+  end
+end
+
+                
