04e8e3ecfd70d41b50b0145ee5752f213c57911a
Nathan Smith <nathansmith22@gmail.com>
2013-02-18 22:03:50 -0500

    RE944 -- added segment W08.

---
+++ lib/stupidedi/versions/functional_groups/004010/segment_defs/W08.rb
@@ -0,0 +1,25 @@
+module Stupidedi
+  module Versions
+    module FunctionalGroups
+      module FortyTen
+        module SegmentDefs
+
+          s = Schema
+          e = ElementDefs
+          r = ElementReqs
+
+          W08 = s::SegmentDef.build(:W08, "Receipt Carrier Information",
+            "To identify carrier equipment and condition.",
+            e::E91.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E140.simple_use(r::Situational, s::RepeatCount.bounded(1)),
+            e::E387.simple_use(r::Optional, s::RepeatCount.bounded(1)),
+            e::E206.simple_use(r::Situational, s::RepeatCount.bounded(1)),
+            e::E207.simple_use(r::Situational, s::RepeatCount.bounded(1)),
+            e::E225.simple_use(r::Optional, s::RepeatCount.bounded(2))
+          )
+        end
+      end
+    end
+  end
+end
+
