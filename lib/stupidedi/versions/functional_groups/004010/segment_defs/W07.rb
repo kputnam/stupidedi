7150a3b2bbfa1867592e0cf8c8bd0543d4ec63a8
Nathan Smith <nathansmith22@gmail.com>
2013-02-18 22:07:33 -0500

    RE944 -- added segment W07.

---
+++ lib/stupidedi/versions/functional_groups/004010/segment_defs/W07.rb
@@ -0,0 +1,26 @@
+module Stupidedi
+  module Versions
+    module FunctionalGroups
+      module FortyTen
+        module SegmentDefs
+
+          s = Schema
+          e = ElementDefs
+          r = ElementReqs
+
+          W07 = s::SegmentDef.build(:W07, "Item Detail For Stock Receipt",
+            "To indicate quantity and condition of product received",
+            e::E413.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E355.simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
+            e::E438.simple_use(r::Situational, s::RepeatCount.bounded(1)),
+            e::E235.simple_use(r::Situational, s::RepeatCount.bounded(1)),
+            e::E234.simple_use(r::Situational, s::RepeatCount.bounded(1)),
+            e::E451.simple_use(r::Optional, s::RepeatCount.bounded(1))
+
+          )
+        end
+      end
+    end
+  end
+end
+
