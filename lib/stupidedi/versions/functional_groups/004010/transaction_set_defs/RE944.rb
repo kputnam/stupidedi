3bf8fe93727f5e4c77b50f0548d978b9896c5190
Nathan Smith <nathansmith22@gmail.com>
2013-02-18 23:06:41 -0500

    RE944 -- added guide.

--- lib/stupidedi/versions/functional_groups/004010/transaction_set_defs/RE944.rb
+++ lib/stupidedi/versions/functional_groups/004010/transaction_set_defs/RE944.rb
@@ -21,16 +21,16 @@ module Stupidedi
 
             d::TableDef.header("Table 2 - Detail",
               d::LoopDef.build("W07", d::RepeatCount.bounded(9999),
-                s::W07.use(220, r::Mandatory, d::RepeatCount.bounded(1)),
-                s::G69.use(230, r::Mandatory, d::RepeatCount.bounded(5)),
-                s::N9.use(240, r::Mandatory, d::RepeatCount.bounded(200)),
-                s::N9.use(240, r::Mandatory, d::RepeatCount.bounded(200))
+                s::W07.use(20, r::Mandatory, d::RepeatCount.bounded(1)),
+                s::G69.use(30, r::Mandatory, d::RepeatCount.bounded(5)),
+                s::N9.use(40, r::Mandatory, d::RepeatCount.bounded(200)),
+                s::N9.use(40, r::Mandatory, d::RepeatCount.bounded(200))
               )
             ),
 
             d::TableDef.header("Table 3 - Summary",
-              s::W14.use( 10, r::Mandatory,  d::RepeatCount.bounded(1)),
-              s::SE.use(30, r::Mandatory, d::RepeatCount.bounded(1))
+              s::W14.use(10, r::Mandatory,  d::RepeatCount.bounded(1)),
+              s::SE.use(20, r::Mandatory, d::RepeatCount.bounded(1))
             )
           )
         end
