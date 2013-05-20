29bf6f98cba594148c01be108c60bd34caf27641
kputnam <putnam.kyle@gmail.com>
2013-05-20 05:58:11 -0500

    Create failing tests

--- spec/support/matchers/either_matchers.rb
+++ spec/support/matchers/either_matchers.rb
@@ -23,4 +23,31 @@ module EitherMatchers
     end
   end
 
+  def be_success(value)
+    BeSuccess.new(value)
+  end
+
+  class BeSuccess
+    def initialize(value)
+      @value = value
+    end
+
+    def matches?(either)
+      @either = either
+      either.select{|x| x == @value}.defined?
+    end
+
+    def failure_message_for_should
+      "expected Either.success(#{@value.inspect}), got #{@either.inspect}"
+    end
+
+    def failure_message_for_should_not
+      "expected non-Either.success(#{@value.inspect}) got #{@either.inspect}"
+    end
+
+    def description
+      "be defined"
+    end
+  end
+
 end
