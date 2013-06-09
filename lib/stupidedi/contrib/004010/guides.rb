b1e3e16a489731e12ab98be53883e6488cbf4bf7
kputnam <putnam.kyle@gmail.com>
2013-06-09 01:59:55 -0500

    Create contrib namespace

---
+++ lib/stupidedi/contrib/004010/guides.rb
@@ -0,0 +1,22 @@
+module Stupidedi
+  module Contrib
+    module FortyTen
+      module Guides
+
+        GuideBuilder = Stupidedi::Guides::FortyTen::GuideBuilder
+        SegmentReqs  = Stupidedi::Guides::FortyTen::SegmentReqs
+        ElementReqs  = Stupidedi::Guides::FortyTen::ElementReqs
+
+        autoload :PO850, "stupidedi/contrib/004010/guides/PO850"
+        autoload :OW940, "stupidedi/contrib/004010/guides/OW940"
+        autoload :AR943, "stupidedi/contrib/004010/guides/AR943"
+        autoload :RE944, "stupidedi/contrib/004010/guides/RE944"
+        autoload :SW945, "stupidedi/contrib/004010/guides/SW945"
+        autoload :SM204, "stupidedi/contrib/004010/guides/SM204"
+        autoload :QM214, "stupidedi/contrib/004010/guides/QM214"
+        autoload :GF990, "stupidedi/contrib/004010/guides/GF990"
+
+      end
+    end
+  end
+end
--- lib/stupidedi/versions/functional_groups/004010/transaction_set_defs.rb
+++ lib/stupidedi/versions/functional_groups/004010/transaction_set_defs.rb
@@ -4,41 +4,6 @@ module Stupidedi
       module FortyTen
         module TransactionSetDefs
 
-          # 004010X... Purchase Order
-          autoload :PO850, # Purchase Order
-            "stupidedi/versions/functional_groups/004010/transaction_set_defs/PO850"
-
-          # 004010X... Warehouse Shipping Order
-          autoload :OW940, # Warehouse Shipping Order
-            "stupidedi/versions/functional_groups/004010/transaction_set_defs/OW940"
-
-          # 004010X... Warehouse Stock Transfer
-          autoload :AR943, # Warehouse Stock Transfer
-            "stupidedi/versions/functional_groups/004010/transaction_set_defs/AR943"
-
-          # 004010X... Warehouse Stock Transfer Receipt Advice
-          autoload :RE944, # Warehouse Stock Transfer Receipt Advice
-            "stupidedi/versions/functional_groups/004010/transaction_set_defs/RE944"
-
-          # 004010X... Warehouse Shipping Advice
-          autoload :SW945, # Warehouse Shipping Advice
-            "stupidedi/versions/functional_groups/004010/transaction_set_defs/SW945"
-
-          # 004010X... Functional Acknowledgement
-          autoload :FA997, #Functional Acknowledgement
-            "stupidedi/versions/functional_groups/004010/transaction_set_defs/FA997"
-
-          # 004010X... Motor Carrier Load Tender
-          autoload :SM204, # Motor Carrier Load Tender
-            "stupidedi/versions/functional_groups/004010/transaction_set_defs/SM204"
-          #
-          # 004010X... Transportation Carrier Shipment Status Message
-          autoload :QM214,
-            "stupidedi/versions/functional_groups/004010/transaction_set_defs/QM214"
-
-          # 004010X... Response to a Load Tender
-          autoload :GF990, # Response to a Load Tender
-            "stupidedi/versions/functional_groups/004010/transaction_set_defs/GF990"
         end
       end
     end
