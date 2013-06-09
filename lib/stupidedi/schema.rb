838d72b3f581374e0b5e911e098b4f2140891440
kputnam <putnam.kyle@gmail.com>
2013-06-09 02:02:32 -0500

    Cleanup formatting

--- lib/stupidedi/schema.rb
+++ lib/stupidedi/schema.rb
@@ -1,7 +1,7 @@
 module Stupidedi
   module Schema
-    autoload :AbstractDef,  "stupidedi/schema/abstract_def"
-    autoload :AbstractUse,  "stupidedi/schema/abstract_use"
+    autoload :AbstractDef,          "stupidedi/schema/abstract_def"
+    autoload :AbstractUse,          "stupidedi/schema/abstract_use"
 
     autoload :CodeList,             "stupidedi/schema/code_list"
     autoload :ElementReq,           "stupidedi/schema/element_req"
@@ -13,16 +13,16 @@ module Stupidedi
     autoload :CompositeElementUse,  "stupidedi/schema/element_use"
     autoload :ComponentElementUse,  "stupidedi/schema/element_use"
 
-    autoload :LoopDef,      "stupidedi/schema/loop_def"
-    autoload :RepeatCount,  "stupidedi/schema/repeat_count"
-    autoload :SegmentDef,   "stupidedi/schema/segment_def"
-    autoload :SegmentReq,   "stupidedi/schema/segment_req"
-    autoload :SegmentUse,   "stupidedi/schema/segment_use"
-    autoload :SyntaxNote,   "stupidedi/schema/syntax_note"
-    autoload :TableDef,     "stupidedi/schema/table_def"
+    autoload :LoopDef,              "stupidedi/schema/loop_def"
+    autoload :RepeatCount,          "stupidedi/schema/repeat_count"
+    autoload :SegmentDef,           "stupidedi/schema/segment_def"
+    autoload :SegmentReq,           "stupidedi/schema/segment_req"
+    autoload :SegmentUse,           "stupidedi/schema/segment_use"
+    autoload :SyntaxNote,           "stupidedi/schema/syntax_note"
+    autoload :TableDef,             "stupidedi/schema/table_def"
 
-    autoload :TransactionSetDef,  "stupidedi/schema/transaction_set_def"
-    autoload :FunctionalGroupDef, "stupidedi/schema/functional_group_def"
-    autoload :InterchangeDef,     "stupidedi/schema/interchange_def"
+    autoload :TransactionSetDef,    "stupidedi/schema/transaction_set_def"
+    autoload :FunctionalGroupDef,   "stupidedi/schema/functional_group_def"
+    autoload :InterchangeDef,       "stupidedi/schema/interchange_def"
   end
 end
