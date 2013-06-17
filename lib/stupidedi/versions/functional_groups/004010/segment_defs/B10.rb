9d2e78e197322ec3204ff9a3dfa865f58c08bb29
kputnam <putnam.kyle@gmail.com>
2013-06-17 09:18:01 -0500

    Remove elements in B10 and L11 not defined in 4010 (GH-12)

--- lib/stupidedi/versions/functional_groups/004010/segment_defs/B10.rb
+++ lib/stupidedi/versions/functional_groups/004010/segment_defs/B10.rb
@@ -17,13 +17,10 @@ module Stupidedi
             e::E128 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
             e::E127 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
             e::E1073.simple_use(r::Optional,   s::RepeatCount.bounded(1)),
-            e::E373 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
-            e::E337 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
 
             SyntaxNotes::R.build(1, 6),
             SyntaxNotes::E.build(1, 5),
-            SyntaxNotes::P.build(5, 6),
-            SyntaxNotes::P.build(8, 9))
+            SyntaxNotes::P.build(5, 6))
 
         end
       end
