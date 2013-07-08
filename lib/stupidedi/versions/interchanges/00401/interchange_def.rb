69daf317f9a5ea1944ed0474d89e4d5f5c4857cf
kputnam <putnam.kyle@gmail.com>
2013-07-07 23:16:41 -0500

    Component and repitition separators no longer must be specified in ISA elements, and can be overridden by passing a Reader::Separators parameter to Writers::Default.new

--- lib/stupidedi/versions/interchanges/00401/interchange_def.rb
+++ lib/stupidedi/versions/interchanges/00401/interchange_def.rb
@@ -31,6 +31,29 @@ module Stupidedi
           def separators(isa)
             Reader::Separators.new(isa.element(16).to_s, nil, nil, nil)
           end
+
+          # @return [SegmentVal]
+          def replace_separators(isa, separators)
+            isa.copy \
+              :separators => separators,
+              :children   =>
+              [isa.element(1),
+               isa.element(2),
+               isa.element(3),
+               isa.element(4),
+               isa.element(5),
+               isa.element(6),
+               isa.element(7),
+               isa.element(8),
+               isa.element(9),
+               isa.element(10),
+               isa.element(11),
+               isa.element(12),
+               isa.element(13),
+               isa.element(14),
+               isa.element(15),
+               isa.element(16).copy(:value => separators.component)]
+          end
         end.new "00401",
           # Interchange header
           [ SegmentDefs::ISA.use(1, r::Mandatory, s::RepeatCount.bounded(1)),
