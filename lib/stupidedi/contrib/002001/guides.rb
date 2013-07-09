58d96f559ca3025a72e0e75017baa2994106f6c4
irobayna <irobayna@gmail.com>
2013-07-09 17:05:52 -0400

    New Grammar ANSI X12 002001
    856 Ship Notice/Manifest

---
+++ lib/stupidedi/contrib/002001/guides.rb
@@ -0,0 +1,15 @@
+module Stupidedi
+  module Contrib
+    module TwoThousandOne
+      module Guides
+
+        GuideBuilder = Stupidedi::Guides::TwoThousandOne::GuideBuilder
+        SegmentReqs  = Stupidedi::Guides::TwoThousandOne::SegmentReqs
+        ElementReqs  = Stupidedi::Guides::TwoThousandOne::ElementReqs
+
+        autoload :SH856, "stupidedi/contrib/002001/guides/SH856"
+
+      end
+    end
+  end
+end
---
+++ lib/stupidedi/contrib/002001/transaction_set_defs.rb
@@ -0,0 +1,15 @@
+module Stupidedi
+  module Contrib
+    module TwoThousandOne
+      module TransactionSetDefs
+
+        SegmentReqs = Versions::FunctionalGroups::TwoThousandOne::SegmentReqs
+        SegmentDefs = Versions::FunctionalGroups::TwoThousandOne::SegmentDefs
+
+        autoload :SH856, # Ship Notice/Manifest
+          "stupidedi/contrib/004010/transaction_set_defs/SH856"
+
+      end
+    end
+  end
+end
