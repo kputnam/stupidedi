a0b3d6e3f208624148d636779042660113d49893
irobayna <irobayna@gmail.com>
2013-07-10 15:08:01 -0400

    first draft guide + tweaks to transaction_set_defs

--- lib/stupidedi/contrib/002001/guides.rb
+++ lib/stupidedi/contrib/002001/guides.rb
@@ -3,10 +3,14 @@ module Stupidedi
     module TwoThousandOne
       module Guides
 
-        GuideBuilder = Stupidedi::Guides::TwoThousandOne::GuideBuilder
-        SegmentReqs  = Stupidedi::Guides::TwoThousandOne::SegmentReqs
-        ElementReqs  = Stupidedi::Guides::TwoThousandOne::ElementReqs
+        # GuideBuilder = Stupidedi::Guides::TwoThousandOne::GuideBuilder
+        # SegmentReqs  = Stupidedi::Guides::TwoThousandOne::SegmentReqs
+        # ElementReqs  = Stupidedi::Guides::TwoThousandOne::ElementReqs
 
+        GuideBuilder = Stupidedi::Guides::FortyTen::GuideBuilder
+        SegmentReqs  = Stupidedi::Guides::FortyTen::SegmentReqs
+        ElementReqs  = Stupidedi::Guides::FortyTen::ElementReqs
+        
         autoload :SH856, "stupidedi/contrib/002001/guides/SH856"
 
       end
