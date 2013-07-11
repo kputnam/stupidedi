d4406343c695057f084a0ab78234d4390d88c58e
irobayna <irobayna@gmail.com>
2013-07-11 14:56:12 -0400

    SH 856 / 002001 support

---
+++ lib/stupidedi/contrib/002001.rb
@@ -0,0 +1,13 @@
+module Stupidedi
+  module Contrib
+    module TwoThousandOne
+
+      autoload :Guides,
+        "stupidedi/contrib/002001/guides"
+
+      autoload :TransactionSetDefs,
+        "stupidedi/contrib/002001/transaction_set_defs"
+
+    end
+  end
+end
