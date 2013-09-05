5202fe64136d91c462b7ebe2b8f1b9ddb59d15ff
irobayna <irobayna@gmail.com>
2013-09-05 09:13:39 -0400

    work in progress

--- lib/stupidedi/versions/functional_groups/003010/segment_defs/BPS.rb
+++ lib/stupidedi/versions/functional_groups/003010/segment_defs/BPS.rb
@@ -14,7 +14,7 @@ module Stupidedi
             "To (1) indicate the beginning of a payment order/remittance advice transaction set and
             total payment and (2) to enable related transfer of funds and/or information from payor to
             payee to occur while utilizing an automated clearing house (ACH) or other banking network.",
-            e::E591.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E591.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)), 
             e::E782.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
             e::E305.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)), #E373 ?
             e::E508.simple_use(r::Optional,   s::RepeatCount.bounded(1)),
