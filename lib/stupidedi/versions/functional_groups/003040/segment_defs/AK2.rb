ca4e136e9a60548f9c835a69ffee66c8b8081072
irobayna <irobayna@gmail.com>
2013-09-10 17:24:39 -0400

    WA142 - Product Service Claim

---
+++ lib/stupidedi/versions/functional_groups/003040/segment_defs/AK2.rb
@@ -0,0 +1,21 @@
+module Stupidedi
+  module Versions
+    module FunctionalGroups
+      module ThirtyTen
+        module SegmentDefs
+
+          s = Schema
+          e = ElementDefs
+          r = ElementReqs
+
+          AK2 = s::SegmentDef.build(:AK2, "Transaction Set Response Header",
+            "To start acknowledgement of a single transaction set",
+            e::E143 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E329 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)))
+
+        end
+      end
+    end
+  end
+end
+
---
+++ lib/stupidedi/versions/functional_groups/003040/segment_defs/BPS.rb
@@ -0,0 +1,27 @@
+module Stupidedi
+  module Versions
+    module FunctionalGroups
+      module ThirtyTen
+        module SegmentDefs
+
+          s = Schema
+          e = ElementDefs
+          r = ElementReqs
+
+        # Definition might be outdated, working from ANSI X12 2001 specification
+        
+          BPS = s::SegmentDef.build(:BPS, "Beginning Segment for Payment Order/Remittance Advice",
+            "To (1) indicate the beginning of a payment order/remittance advice transaction set and
+            total payment and (2) to enable related transfer of funds and/or information from payor to
+            payee to occur while utilizing an automated clearing house (ACH) or other banking network.",
+            e::E591.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)), 
+            e::E782.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E305.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)), #E373 ?
+            e::E508.simple_use(r::Optional,   s::RepeatCount.bounded(1)),
+            e::E513.simple_use(r::Optional,  s::RepeatCount.bounded(1))) 
+
+        end
+      end
+    end
+  end
+end
