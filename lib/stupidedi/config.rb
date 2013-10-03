f8b979f44bbd3723c08774d8a51653495c12377d
Isi Robayna <isi.robayna@ctg.com>
2013-10-03 09:35:22 -0400

    PS 830 v4010 parses correctly now

--- lib/stupidedi/config.rb
+++ lib/stupidedi/config.rb
@@ -123,22 +123,16 @@ module Stupidedi
           x.register("004010", "QM", "214") { Stupidedi::Contrib::FortyTen::TransactionSetDefs::QM214 }
           x.register("004010", "GF", "990") { Stupidedi::Contrib::FortyTen::TransactionSetDefs::GF990 }
           x.register("004010", "SS", "862") { Stupidedi::Contrib::FortyTen::TransactionSetDefs::SS862 }
-          x.register("004010", "PS", "830") { Stupidedi::Contrib::FortyTen::Guides::PS830 }
-          # x.register("004010", "PS", "830") { Stupidedi::Contrib::FortyTen::TransactionSetDefs::PS830 }
+          x.register("004010", "PS", "830") { Stupidedi::Contrib::FortyTen::TransactionSetDefs::PS830 }
           x.register("004010", "SH", "856") { Stupidedi::Contrib::FortyTen::TransactionSetDefs::SH856 }
           x.register("004010", "SQ", "866") { Stupidedi::Contrib::FortyTen::TransactionSetDefs::SQ866 }
 
-          #x.register("002001", "SH", "856") { Stupidedi::Contrib::TwoThousandOne::TransactionSetDefs::SH856 }
-
           x.register("002001", "SH", "856") { Stupidedi::Contrib::TwoThousandOne::TransactionSetDefs::SH856 }         
-
           x.register("002001", "PO", "830") { Stupidedi::Contrib::TwoThousandOne::TransactionSetDefs::PO830 }         
 
           x.register("003010", "RA", "820") { Stupidedi::Contrib::ThirtyTen::Guides::RA820 }
-          
           # x.register("003010", "PO", "850") { Stupidedi::Contrib::ThirtyTen::Guides::PO850 }  # OK
-          x.register("003010", "PO", "850") { Stupidedi::Contrib::ThirtyTen::TransactionSetDefs::PO850 }  # non-deterministic result
-          
+          x.register("003010", "PO", "850") { Stupidedi::Contrib::ThirtyTen::TransactionSetDefs::PO850 }  # non-deterministic result        
           x.register("003010", "PC", "860") { Stupidedi::Contrib::ThirtyTen::Guides::PC860 }
 
           x.register("003040", "WA", "142") { Stupidedi::Contrib::ThirtyForty::Guides::WA142 }
--- lib/stupidedi/contrib/004010/transaction_set_defs/PS830.rb
+++ lib/stupidedi/contrib/004010/transaction_set_defs/PS830.rb
@@ -31,22 +31,9 @@ module Stupidedi
                 s::FST.use( 460, r::Optional,  d::RepeatCount.bounded(6))),
 
               d::LoopDef.build("SHP", d::RepeatCount.bounded(1),
-                s::SHP.use( 470, r::Optional,  d::RepeatCount.bounded(1)))),
-
-            d::LoopDef.build("LIN", d::RepeatCount.unbounded,
-              s::LIN.use( 10, r::Optional, d::RepeatCount.bounded(1)),
-              s::UIT.use( 20, r::Optional, d::RepeatCount.bounded(1)),
-              s::PID.use( 80, r::Optional, d::RepeatCount.bounded(1)),
-
-              d::LoopDef.build("N1", d::RepeatCount.bounded(1),
-                s::N1.use( 320, r::Optional,  d::RepeatCount.bounded(1))),
-
-              d::LoopDef.build("SDP", d::RepeatCount.bounded(260),
-                s::SDP.use( 450, r::Optional,  d::RepeatCount.bounded(1)),
-                s::FST.use( 460, r::Optional,  d::RepeatCount.bounded(1))),
-
-              d::LoopDef.build("SHP", d::RepeatCount.bounded(1),
                 s::SHP.use( 470, r::Optional,  d::RepeatCount.bounded(1))))),
+
+
 
           d::TableDef.header("Table 3 - Summary",
             s::CTT.use( 10, r::Optional,  d::RepeatCount.bounded(1)),
