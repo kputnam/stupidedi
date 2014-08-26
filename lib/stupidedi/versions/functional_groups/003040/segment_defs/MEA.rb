a6484f3eeba8f06c40860c42f4514d6ecf84b468
kputnam <putnam.kyle@gmail.com>
2014-08-26 18:42:51 -0500

    Improve validation of SyntaxNotes and remove invalid MEA notes (closes GH-52)

--- lib/stupidedi/versions/functional_groups/003040/segment_defs/MEA.rb
+++ lib/stupidedi/versions/functional_groups/003040/segment_defs/MEA.rb
@@ -15,16 +15,15 @@ module Stupidedi
             e::E739 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
             e::E355 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
             e::E740 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
-            e::E741 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
+            e::E741 .simple_use(r::Relational, s::RepeatCount.bounded(1)))
 
-            SyntaxNotes::R.build( 3,  5,  6,  8),
-            SyntaxNotes::E.build( 4, 12),
-            SyntaxNotes::L.build( 5,  4, 12),
-            SyntaxNotes::L.build( 6,  4, 12),
-            SyntaxNotes::L.build( 7,  3,  5,  6),
-            SyntaxNotes::E.build( 8,  3),
-            SyntaxNotes::P.build(11, 12))
-
+          # SyntaxNotes::R.build( 3,  5,  6,  8),
+          # SyntaxNotes::E.build( 4, 12),
+          # SyntaxNotes::L.build( 5,  4, 12),
+          # SyntaxNotes::L.build( 6,  4, 12),
+          # SyntaxNotes::L.build( 7,  3,  5,  6),
+          # SyntaxNotes::E.build( 8,  3),
+          # SyntaxNotes::P.build(11, 12))
 
         end
       end
