ddf7f4b6e186e9ce693304e5ddf6b4c983580f62
Karl Falconer <karl.falconer@falconerdevelopment.com>
2014-09-30 15:39:04 -0400

    added SCH and N1 Loops to the PO Details

---
+++ lib/stupidedi/versions/functional_groups/004010/segment_defs/SCH.rb
@@ -0,0 +1,28 @@
+module Stupidedi
+  module Versions
+    module FunctionalGroups
+      module FortyTen
+        module SegmentDefs
+
+          s = Schema
+          e = ElementDefs
+          r = ElementReqs
+
+          SCH = s::SegmentDef.build(:SCH, "Line Item Schedule",
+            "To specify the dat for scheduling aspecifc line-item",
+            e::E380 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E355 .simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
+            e::E98  .simple_use(r::Optional,   s::RepeatCount.bounded(1)),
+            e::E93  .simple_use(r::Relational,   s::RepeatCount.bounded(1)),
+            e::E374 .simple_use(r::Optional,   s::RepeatCount.bounded(1)),
+            e::E373 .simple_use(r::Optional,   s::RepeatCount.bounded(1)),
+            e::E374 .simple_use(r::Relational,   s::RepeatCount.bounded(1)),
+            e::E373 .simple_use(r::Relational,   s::RepeatCount.bounded(1)),
+
+            SyntaxNotes::C.build(3, 4))
+
+        end
+      end
+    end
+  end
+end
