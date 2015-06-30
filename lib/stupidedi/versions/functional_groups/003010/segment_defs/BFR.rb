c0f853dbcb5ffd1f44a36b6c5165f1f743e5e6a7
Isi Robayna <isi.robayna@ctg.com>
2015-06-30 13:45:44 +0100

    Update BFR segment for v3010

--- lib/stupidedi/versions/functional_groups/003010/segment_defs/BFR.rb
+++ lib/stupidedi/versions/functional_groups/003010/segment_defs/BFR.rb
@@ -11,15 +11,27 @@ module Stupidedi
           # Definition might be outdated, working from ANSI X12 2001 specification
           BFR = s::SegmentDef.build(:BFR, "Beginning Segment for Planning Schedule",
             "To indicate the beginning of a planning schedule transaction set; whether a ship or "\
-            "delivery based forecast; and related forecast envelope datess",
-            e::E353.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
-            e::E127.simple_use(r::Relational, s::RepeatCount.bounded(1)),
-            e::E328.simple_use(r::Relational, s::RepeatCount.bounded(1)),
-            e::E675.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
-            e::E676.simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
-            e::E373.simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
-            e::E373.simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
-            e::E373.simple_use(r::Mandatory, s::RepeatCount.bounded(1)))
+            "delivery based forecast; and related forecast envelope dates",
+            e::E353 .simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
+            e::E127 .simple_use(r::Relational,s::RepeatCount.bounded(1)),
+            e::E328 .simple_use(r::Relational,s::RepeatCount.bounded(1)),
+            e::E675 .simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
+            e::E676 .simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
+            e::E373 .simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
+            e::E373 .simple_use(r::Optional,  s::RepeatCount.bounded(1)),
+            e::E373 .simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
+            e::E367 .simple_use(r::Optional,  s::RepeatCount.bounded(1)),
+            e::E367 .simple_use(r::Optional,  s::RepeatCount.bounded(1)),
+            e::E324 .simple_use(r::Optional,  s::RepeatCount.bounded(1)))
+
+            # e::E353.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            # e::E127.simple_use(r::Relational, s::RepeatCount.bounded(1)),
+            # e::E328.simple_use(r::Relational, s::RepeatCount.bounded(1)),
+            # e::E675.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            # e::E676.simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
+            # e::E373.simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
+            # e::E373.simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
+            # e::E373.simple_use(r::Mandatory, s::RepeatCount.bounded(1)))
 
         end
       end
