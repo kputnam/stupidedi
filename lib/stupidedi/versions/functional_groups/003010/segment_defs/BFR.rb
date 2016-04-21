9bb0bd11399396155a328775a652df12891d4167
Michael Tyler Shrout <mtshro2@gmail.com>
2016-04-21 14:17:34 -0400

    Update BFR.rb
    830 BFR Segments have 11 elements, added 10th one (for Contract Number), which was missing.

--- lib/stupidedi/versions/functional_groups/003010/segment_defs/BFR.rb
+++ lib/stupidedi/versions/functional_groups/003010/segment_defs/BFR.rb
@@ -21,7 +21,8 @@ module Stupidedi
             e::E373 .simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
             e::E373 .simple_use(r::Optional,  s::RepeatCount.bounded(1)),
             e::E373 .simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
-            e::E373 .simple_use(r::Mandatory, s::RepeatCount.bounded(1)),            
+            e::E373 .simple_use(r::Mandatory, s::RepeatCount.bounded(1)),
+            e::E367 .simple_use(r::Optional,  s::RepeatCount.bounded(1)),
             e::E324 .simple_use(r::Optional,  s::RepeatCount.bounded(1)))
 
             # e::E353.simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
