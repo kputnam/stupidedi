904afe239f95b49e7eb45eb893ae3da75bda1fe0
Autumn Preskill <autumnpreskill@transfix.io>
2017-07-05 12:38:12 -0400

    Fix spacing issues, add syntax notes

--- lib/stupidedi/versions/functional_groups/004010/segment_defs/OID.rb
+++ lib/stupidedi/versions/functional_groups/004010/segment_defs/OID.rb
@@ -11,17 +11,20 @@ module Stupidedi
 
           OID = s::SegmentDef.build(:OID, "Order identification",
             "Order Identification Detail",
-            e::E127 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
+            e::E211 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
             e::E324 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
             e::E355 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
             e::E380 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
             e::E188 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
-            e::E81 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
+            e::E81  .simple_use(r::Relational, s::RepeatCount.bounded(1)),
             e::E184 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
-            e::E183 .simple_use(r::Relational, s::RepeatCount.bounded(1)))
+            e::E183 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
 
-            # SyntaxNotes::P.build(6, 7),
-            # SyntaxNotes::P.build(1, 2, 3))
+            SyntaxNotes::R.build(1, 2),
+            SyntaxNotes::C.build(3, 2),
+            SyntaxNotes::P.build(4, 5),
+            SyntaxNotes::P.build(6, 7),
+            SyntaxNotes::P.build(8, 9))
 
         end
       end
