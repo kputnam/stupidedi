ccf7b93b9b962ab962212149d8521bd9052b5ce1
jeff.marek <jeff.marek@erisfutures.com>
2018-09-07 11:00:50 -0500

    co470 spec support for AG824

---
+++ lib/stupidedi/contrib/004010/transaction_set_defs/AG824.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Stupidedi
+  module Contrib
+    module FortyTen
+      module TransactionSetDefs
+
+        d = Schema
+        r = SegmentReqs
+        s = SegmentDefs
+
+        RA820 = d::TransactionSetDef.build("AG", "824", "Application Advice",
+          d::TableDef.header("Table 1 - Header",
+            s:: ST.use(10, r::Mandatory, d::RepeatCount.bounded(1)),
+            s::BGN.use(20, r::Mandatory, d::RepeatCount.bounded(1)),
+            d::LoopDef.build("N1", d::RepeatCount.unbounded,
+              s::N1.use(30, r::Mandatory, d::RepeatCount.bounded(1)),
+              s::PER.use(80, r::Mandatory, d::RepeatCount.bounded(3)),
+            )
+          ),
+          d::TableDef.detail("Table 2 - Detail",
+            d::LoopDef.build("OTI", d::RepeatCount.unbounded,
+              s::OTI.use(10, r::Mandatory,  d::RepeatCount.bounded(1)),
+              s::AMT.use(50, r::Optional,  d::RepeatCount.unbounded),
+              s::QTY.use(60, r::Optional,  d::RepeatCount.unbounded),
+              d::LoopDef.build("OTI", d::RepeatCount.unbounded,
+                s::OTI.use(65, r::Optional,  d::RepeatCount.bounded(1)),
+                s::AMT.use(67, r::Optional,  d::RepeatCount.bounded(1)),
+                s::TED.use(70, r::Optional,  d::RepeatCount.bounded(1)),
+              ),
+            ),
+             s:: SE.use(90, r::Mandatory, d::RepeatCount.bounded(1))
+          )
+        )
+
+      end
+    end
+  end
+end
