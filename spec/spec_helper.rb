771db19eba5bc6b10ea1e17b3a518572c25c5e71
kputnam <kputnam@users.noreply.github.com>
2019-01-12 22:14:59 -0600

    Load SimpleCov first so lib/ruby and others are tracked

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,30 @@
+# The SimpleCov.start must be issued before any of your application code is required!
+begin
+  require "simplecov"
+  SimpleCov.start do
+    add_filter %r{/segment_defs}
+    add_filter %r{/element_defs}
+    add_filter %r{/spec}
+    add_filter %r{/transaction_sets/[0-9]+/[A-Z0-9-]+\.rb}
+
+    add_group "Refinements",      "lib/ruby"
+    add_group "Config",           "lib/stupidedi/config"
+    add_group "Editor",           "lib/stupidedi/editor"
+    add_group "Exceptions",       "lib/stupidedi/exceptions"
+    add_group "Interchanges",     "lib/stupidedi/interchanges"
+    add_group "Parser",           "lib/stupidedi/parser"
+    add_group "Reader",           "lib/stupidedi/reader"
+    add_group "Schema",           "lib/stupidedi/schema"
+    add_group "TransactionSets",  "lib/stupidedi/transaction_sets"
+    add_group "Values",           "lib/stupidedi/values"
+    add_group "Versions",         "lib/stupidedi/versions"
+    add_group "Writer",           "lib/stupidedi/writer"
+    add_group "Zipper",           "lib/stupidedi/zipper"
+  end
+rescue LoadError
+end
+
 require File.expand_path("../../lib/stupidedi", __FILE__)
-require "#{File.dirname(__FILE__)}/support/coverage.rb"
 require "pp"
 
 begin
