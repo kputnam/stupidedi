330a46f618bb41acb5e0011f3b05442bd225eaac
kputnam <kputnam@users.noreply.github.com>
2019-01-18 00:09:23 -0600

    Fix deprecation warnings for  X221::HP835, X222::HC837, X222::HC837P, and X222A1::HC837P, X223A1::HC837, X220::BE834

--- lib/stupidedi/transaction_sets/005010/implementations/X222-HC837P.rb
+++ lib/stupidedi/transaction_sets/005010/implementations/X222-HC837P.rb
@@ -4,10 +4,10 @@ module Stupidedi
     module FiftyTen
       module Implementations
         module X222
-          warn "#{self}::HC837P is deprecated, use HC837 instead"
+          warn "#{self}::HC837P is deprecated, use X22A1::HC837 instead"
 
           # @deprecated
-          HC837P = HC837
+          HC837P = X222A1::HC837
         end
       end
     end
--- lib/stupidedi/transaction_sets/005010/implementations/X222A1-HC837P.rb
+++ lib/stupidedi/transaction_sets/005010/implementations/X222A1-HC837P.rb
@@ -4,7 +4,7 @@ module Stupidedi
     module FiftyTen
       module Implementations
         module X222A1
-          warn "#{self}::HC837P is deprecated, use HC837 instead"
+          warn "#{self}::HC837P is deprecated, use X22A1::HC837 instead"
 
           # @deprecated Use HC837 instead
           HC837P = HC837
