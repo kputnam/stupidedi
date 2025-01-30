ea2d4a0d53b6a942aada9c511884ce842a6e0351
Adrian Duyzer <adrianduyzer@gmail.com>
2025-01-30 14:32:10 -0500

    add missing optional element (E127) to the 4010 OID

--- lib/stupidedi/versions/004010/segment_defs/OID.rb
+++ lib/stupidedi/versions/004010/segment_defs/OID.rb
@@ -7,21 +7,23 @@ module Stupidedi
         e = ElementDefs
         r = ElementReqs
 
-        OID = s::SegmentDef.build(:OID, "Order identification",
-          "",
-          e::E127 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
-          e::E324 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
-          e::E355 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
-          e::E380 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
-          e::E188 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
-          e::E81  .simple_use(r::Relational, s::RepeatCount.bounded(1)),
-          e::E184 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
-          e::E183 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
+        OID = s::SegmentDef.build(:OID, "Order Identification Detail",
+          "To specify order identification detail",
+          e::E127 .simple_use(r::Relational, s::RepeatCount.bounded(1)), # Min 30
+          e::E324 .simple_use(r::Relational, s::RepeatCount.bounded(1)), # Min 22
+          e::E127 .simple_use(r::Optional, s::RepeatCount.bounded(1)), # Min 30
+          e::E355 .simple_use(r::Relational, s::RepeatCount.bounded(1)), # Min 2
+          e::E380 .simple_use(r::Relational, s::RepeatCount.bounded(1)), # Min 15
+          e::E188 .simple_use(r::Relational, s::RepeatCount.bounded(1)), # Min 1
+          e::E81  .simple_use(r::Relational, s::RepeatCount.bounded(1)), # Min 10
+          e::E184 .simple_use(r::Relational, s::RepeatCount.bounded(1)), # Min 1
+          e::E183 .simple_use(r::Relational, s::RepeatCount.bounded(1)), # Min 8
           SyntaxNotes::R.build(1, 2),
           SyntaxNotes::C.build(3, 2),
           SyntaxNotes::P.build(4, 5),
           SyntaxNotes::P.build(6, 7),
-          SyntaxNotes::P.build(8, 9))
+          SyntaxNotes::P.build(8, 9)
+        )
       end
     end
   end
